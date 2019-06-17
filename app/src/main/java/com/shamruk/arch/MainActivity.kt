7f3db6f5582fb6631237480d9b0d18a43eaf2745
Yura Shamruk <yura.shamruk@monterosa.co.uk>
2019-06-17 11:58:26 +0300

    improved navigation

--- app/src/main/java/com/shamruk/arch/MainActivity.kt
+++ app/src/main/java/com/shamruk/arch/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.shamruk.arch.screen.LoginFragment
 import com.shamruk.arch.screen.MainListFragment
@@ -17,8 +18,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = findNavController(mainContainer)
-        bottomNavigation.setupWithNavController(navController)
-
+//        val navController = findNavController(mainContainer)
+//        bottomNavigation.setupWithNavController(navController)
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        }
     }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Now that BottomNavigationBar has restored its instance state
+        // and its selectedItemId, we can proceed with setting up the
+        // BottomNavigationBar with Navigation
+        setupBottomNavigationBar()
+    }
+
+
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        val navGraphIds = listOf(R.navigation.registration, R.navigation.navigation)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.mainContainer,
+            intent = intent
+        )
+    }
+
 }
